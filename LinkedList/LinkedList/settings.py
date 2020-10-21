4ae5b8b97b85b30a35a306391653aded2f78b980
mballard29 <mmballard29@gmail.com>
2020-10-21 11:35:42 -0400

    Fixed time zone

--- LinkedList/LinkedList/settings.py
+++ LinkedList/LinkedList/settings.py
@@ -21,7 +21,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
@@ -90,7 +89,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
