62399fa8f7f5fa469527ce1dd23b0ca429fe0eff
ryanratkovich <ryanratkovich@gmail.com>
2020-12-02 00:07:47 -0600

    Update settings.py

--- TheLinkedList/TheLinkedList/settings.py
+++ TheLinkedList/TheLinkedList/settings.py
@@ -24,9 +24,6 @@ INSTALLED_APPS = [
     # custom apps
     'api.apps.ApiConfig',
 
-    # 3rd party libraries
-    'crispy_forms',
-
     #preinstalled apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -114,7 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
