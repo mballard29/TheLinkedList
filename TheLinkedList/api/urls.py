70db8d59d15fcaa2b9cc224e29b135d59a61339d
Andres Molina <62624408+am18ah@users.noreply.github.com>
2020-12-02 22:15:49 -0500

    the advanced funciton update

--- TheLinkedList/api/urls.py
+++ TheLinkedList/api/urls.py
@@ -12,29 +12,14 @@ urlpatterns = [
     path('event/new/', views.event_new, name='event_new'),
     path('event/<int:pk>/edit/', views.event_edit, name='event_edit'),
     path('event/<pk>/remove/', views.event_remove, name='event_remove'),
+    path('adventure/', views.adventure, name='adventure'),
+
 
     path('search_user/', views.search_user, name='search_user'),
     path('search_event/', views.search_event, name='search_event'),
     path('search_event_by_user/', views.search_event_by_user, name='search_event_by_user'),
     path('count_users_attending_event/', views.count_users_attending_event, name='count_users_attending_event'),
-    # path('register/', views.register, name='register'),
-    # path('users/<slug>/', views.profile_view, name='profile_view'),
-    # path('my-profile/', views.profile_view, name='my_profile'),
-    # path('edit-profile/', views.edit_profile, name='edit_profile'),
-    # path('search_users/', views.search_users, name='search_users'),
+
+
 ]
 
-'''
-add later
-    path('users/event-invite/send/<int:id>/', user_views.send_event_invite, name='send_event_invite'),
-    path('users/event-invite/accept/<int:id>/', user_views.accept_event_invite, name='accept_event_invite'),
-    path('users/event-invite/cancel/<int:id>/', user_views.cancel_event_invite, name='cancel_event_invite'),
-    path('users/event-invite/reject/<int:id>/', user_views.reject_event_invite, name='delete_event_invite'),
-    path('users/event/delete/<int:id>/', user_views.delete_event, name='delete_event'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
-    path('password-reset/done/', auth_views.PasswordResetView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
-'''
